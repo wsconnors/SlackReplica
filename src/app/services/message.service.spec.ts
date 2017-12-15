9cf569cac7e9e8ff35f74c4886b980f82d73f6e5
wesley <wesley@zipcoders-MacBook-Pro-8.local>
2017-12-15 11:30:56 -0500

    comments fix

--- src/app/services/message.service.spec.ts
+++ src/app/services/message.service.spec.ts
@@ -21,20 +21,6 @@ describe('MessageService', () => {
     expect(service).toBeTruthy();
   });
 
-  //
-  // it('should add a message to the message service', () => {
-  //
-  // });
-  //
-  // it('should get message', () => {
-  //   service.addMessageToService(testMessage1);
-  //   console.log(service.getMessage().map(val => val.json());
-  //   service.getMessage().toPromise().then( (result) =>{
-  //     console.log("result: "+result)
-  //     //expect(result.message).toEqual("helo")
-  //   });
-  // });
-
   it('should get message by messageId (message exist)', () =>{
     expect(service.getMessageByMessageId(0).message).toEqual('hello');
   });
@@ -43,12 +29,7 @@ describe('MessageService', () => {
     expect(service.getMessageByMessageId(10)).toBeUndefined();
   });
 
-  // it('should delete message by messageId', () =>{
-  //   service.deleteMessageByMessageId(0);
-  //   expect(service.getMessages()[0].message).toEqual('world');
-  // });
   it('should edit message by messageId (message does exist)', () => {
-    // expect(service.editMessageByMessageId(0, 'goodbye') && service.getMessageByMessageId(0).message == 'goodbye').toBeTruthy();
     service.editMessageByMessageId(0, 'goodbye');
     expect(service.getMessageByMessageId(0).message).toEqual('goodbye');
   });
@@ -56,32 +37,4 @@ describe('MessageService', () => {
     expect(service.editMessageByMessageId(14, 'goodbye')).toBeFalsy();
   });
 
-  // it('should add message to service', () => {
-  //
-  // })
-
-
-
-
 });
-
-
-// it('should get all messages', () =>{
-//   expect(service.getAllMessages()[0].message).toEqual('hello');
-// });
-//
-
-//
-// it('should add message', () =>{
-//   expect(service.getAllMessages()[1].message).toEqual('world');
-// });
-//
-
-//
-// it('should get all messages by the specific date', () => {
-//   expect(service.getMessagesbySpecificDay(2017, 12, 14)).toEqual(service.getAllMessages());
-// });
-
-
-
-//, inject([MessageService],(service: MessageService) =>{}));
