54ea97f89f52f2f5f4e4277dca19401661bdc97f
donald f <donaldf@zipcoders-MacBook-Pro-10.local>
2017-12-15 14:20:14 -0500

    wes made more changes

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,8 +1,7 @@
 import { MessageObj } from '../objects/message-obj'
-<<<<<<< HEAD
-export var MessageObjects:MessageObj[] = [];
-=======
 import { User } from '../objects/user-obj'
+
+
 export var MessageObjects:MessageObj[] = [];
 
 export var Users:User[] = [
@@ -10,4 +9,3 @@ export var Users:User[] = [
   new User("donald","password2"),
   new User("wes","password3")
 ]
->>>>>>> da7249b84edfc8e7cc17d054b789f88f0316eed8
